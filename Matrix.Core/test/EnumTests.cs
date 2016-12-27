b71c30887ee9bfb42610e17f88f2628a21952a22
Alexander Gnauck <alex@ag-software.net>
2016-12-27 03:00:25 -0800

    att tests

---
+++ Matrix.Core/test/EnumTests.cs
@@ -0,0 +1,48 @@
+﻿using Matrix.Core.Attributes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Shouldly;
+
+namespace Matrix.Core.Tests
+{
+    [TestClass]
+    public class EnumTests
+    {
+        enum TestEnum
+        {
+            A = 1,
+            B = 5,
+            C = 11
+        }
+
+        enum Colors
+        {
+            Unknown = -1,
+
+            [Name("rot")]
+            Red = 1,
+
+            [Name("blau")]
+            Blue = 5,
+
+            [Name("gelb")]
+            Yellow = 11
+        }
+
+        [TestMethod]
+        public void GetValuesTest()
+        {
+            Enum.GetValues<TestEnum>().ShouldBe(new int[]{1,5,11});
+        }
+
+        [TestMethod]
+        public void ParseUsingNameAttributeTest()
+        {
+            Enum.ParseUsingNameAttrib<Colors>("rot").ShouldBe(Colors.Red);
+            Enum.ParseUsingNameAttrib<Colors>("gelb").ShouldBe(Colors.Yellow);
+            Enum.ParseUsingNameAttrib<Colors>("blau").ShouldBe(Colors.Blue);
+            Enum.ParseUsingNameAttrib<Colors>("BLAU").ShouldNotBe(Colors.Blue);
+            Enum.ParseUsingNameAttrib<Colors>("BLAU").ShouldBe(Colors.Unknown);
+        }
+    }
+}
---
+++ Matrix.Core/test/JidTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matrix.Core.Tests
+{
+    [TestClass]
+    public class JidTests
+    {
+
+        [TestMethod]
+        public void FullJidTestTest()
+        {
+            var jid1 = "user@server.de/resource";
+            Jid jid = jid1;
+            Assert.AreEqual(jid.ToString(), jid1);
+            Assert.AreEqual((string)jid, jid1);
+        }
+
+        [TestMethod]
+        public void BuilldJidFromUserAndServerTest()
+        {
+            const string user = "user";
+            const string server = "server.com";
+
+            var jid = new Jid { User = user, Server = server };
+            Assert.AreEqual(jid.ToString(), user + "@" + server);
+        }
+
+        [TestMethod]
+        public void JidEscapeTest()
+        {
+            const string user = "gnauck@ag-software.de";
+            const string server = "server.com";
+
+            var jid = new Jid();
+            jid.SetUser(user);
+            Assert.AreEqual(jid.User, @"gnauck\40ag-software.de");
+
+            jid.SetServer(server);
+            Assert.AreEqual(jid.Server, @"server.com");
+            Assert.AreEqual(jid.ToString(), @"gnauck\40ag-software.de@server.com");
+        }
+
+        [TestMethod]
+        public void UppercaseUsernameTest()
+        {
+            const string user = "Gnauck";
+            const string server = "Server.com";
+
+            var jid = new Jid { User = user };
+            Assert.AreEqual(jid.User, user);
+
+            jid.Server = server;
+            Assert.AreEqual(jid.Server, server);
+            Assert.AreEqual(jid.ToString(), user + "@" + server);
+
+            jid.SetUser(user);
+            jid.SetServer(server);
+            Assert.AreEqual(jid.ToString(), "gnauck@server.com");
+        }
+
+        [TestMethod]
+        public void CloneJidTest()
+        {
+            const string JID1 = "user@server.de/resource";
+            const string JID2 = "user2@server.de/resource2";
+
+            Jid jid1 = JID1;
+            var jid2 = jid1.Clone();
+
+            Assert.AreEqual(jid1.ToString(), JID1);
+            Assert.AreEqual(jid2.ToString(), JID1);
+
+            jid2.Resource = "resource2";
+            jid2.User = "user2";
+
+            Assert.AreEqual(jid1.ToString(), JID1);
+            Assert.AreEqual(jid2.ToString(), JID2);
+        }
+
+        [TestMethod]
+        public void CompareJidTest1()
+        {
+            var a = new Jid("User1", "Server.com", "Res01");
+            var b = new Jid("User1@Server.com/Res01");
+
+            var c = new Jid("User1@Server.com/Res01", true);
+
+            Assert.IsTrue(a.CompareTo(b) != 0);
+        }
+
+        [TestMethod]
+        public void CompareJidTest2()
+        {
+            var a = new Jid("User1", "Server.com", "Res01");
+            var b = new Jid("user1@server.com/Res01");
+            Assert.IsTrue(a.CompareTo(b) == 0);
+        }
+
+        [TestMethod]
+        public void CompareJidTest3()
+        {
+            var a = new Jid("User1", "Server.com", "Res01");
+            var b = new Jid("User1@Server.com/Res01");
+            var c = new Jid(b.User, b.Server, b.Resource);
+            Assert.IsTrue(a.CompareTo(c) == 0);
+            Assert.IsTrue(b.CompareTo(c) != 0);
+        }
+    }
+}
