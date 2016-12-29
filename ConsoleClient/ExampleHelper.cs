39597b3c8ac6004dc0fabf43367c8447e2fa7d9a
Alexander Gnauck <alex@ag-software.net>
2016-12-29 14:30:09 +0100

    add example

---
+++ ConsoleClient/ExampleHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotNetty.Common.Internal.Logging;
+
+using Microsoft.Extensions.Logging.Console;
+
+namespace ConsoleClient
+{
+    public static class ExampleHelper
+    {
+        public static void SetConsoleLogger() => InternalLoggerFactory.DefaultFactory.AddProvider(new ConsoleLoggerProvider((s, level) => true, false));
+    }
+}
---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using ConsoleClient;
+using Matrix;
+using Matrix.Xml;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory.RegisterElementsFromAssembly(typeof(Matrix.Xmpp.Client.Iq).GetTypeInfo().Assembly);
+
+            ExampleHelper.SetConsoleLogger();
+
+
+            var xmppClient = new XmppClient()
+            {
+                Username = "alex",
+                Password = "***REMOVED***",
+                XmppDomain = "ag-software.net",
+               
+            };
+
+            xmppClient.ConnectAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+
+}
