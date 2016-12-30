7d4bdf78a9791aa8eaf82f3445da352c19efc0d5
Alexander Gnauck <alex@ag-software.net>
2016-12-30 18:59:22 +0100

    add component classes

---
+++ Matrix.Xmpp/src/Component/Presence.cs
@@ -0,0 +1,40 @@
+using Matrix.Core.Attributes;
+
+namespace Matrix.Xmpp.Component
+{
+    [XmppTag(Name = Tag.Presence, Namespace = Namespaces.Accept)]
+    public class Presence : Base.Presence
+    {
+        #region <<Constructors >>
+        public Presence() : base(Namespaces.Accept)
+        {
+        }
+
+        public Presence(Show show) : this()
+        {
+            Show = show;
+        }
+
+        public Presence(Show show, string status)
+            : this(show)
+        {
+            Status = status;
+        }
+
+        public Presence(Show show, string status, int priority)
+            : this(show, status)
+        {
+            Priority = priority;
+        }
+        #endregion
+
+        /// <summary>
+        /// Error object
+        /// </summary>
+        public Error Error
+        {
+            get { return Element<Error>(); }
+            set { Replace<Error>(value); }
+        }
+    }
+}
