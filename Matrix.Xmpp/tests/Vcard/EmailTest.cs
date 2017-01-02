b01d32bbbcf0e8ffc611739b14c04dd513c580f5
Alexander Gnauck <alex@ag-software.net>
2017-01-02 22:35:10 +0100

    refactor

--- Matrix.Xmpp/tests/Vcard/EmailTest.cs
+++ Matrix.Xmpp/tests/Vcard/EmailTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Matrix.Xml;
 using Matrix.Xmpp.Vcard;
+using Shouldly;
 
 namespace Matrix.Xmpp.Tests.Vcard
 {
@@ -14,21 +15,19 @@ namespace Matrix.Xmpp.Tests.Vcard
         public void Test1()
         {
             XmppXElement xmpp1 = XmppXElement.LoadXml(xml1);
-            Assert.AreEqual(true, xmpp1 is Email);
-
+            xmpp1.ShouldBeOfType<Email>();
+            
             Email email1 = xmpp1 as Email;
-            Assert.AreEqual(email1.Address, "info@ag-software.de");
-            Assert.AreEqual(email1.IsPreferred, false);
+            email1.Address.ShouldBe("info@ag-software.de");
+            email1.IsPreferred.ShouldBeFalse();
 
             XmppXElement xmpp2 = XmppXElement.LoadXml(xml2);
-            Assert.AreEqual(true, xmpp2 is Email);
+            xmpp2.ShouldBeOfType<Email>();
 
             Email email2 = xmpp2 as Email;
-            Assert.AreEqual(email2.Address, "stpeter@jabber.org");
-            Assert.AreEqual(email2.IsPreferred, true);
-
-
-            Email email3 = new Email("info@ag-software.de");
+            email2.Address.ShouldBe("stpeter@jabber.org");
+            email2.IsPreferred.ShouldBeTrue();
+           
             email1.ShouldBe(xml1);
 
             Email email4 = new Email("stpeter@jabber.org", true);
@@ -36,8 +35,3 @@ namespace Matrix.Xmpp.Tests.Vcard
         }
     }
 }
-
-
-
-
-
