b1893bd2e7b981ec85931a6ad884f02c1bca658a
Alex <alex@automotivemastermind.com>
2017-01-13 18:32:01 +0100

    refactor

--- test/Matrix.Tests/Xmpp/Tune/TuneTest.cs
+++ test/Matrix.Tests/Xmpp/Tune/TuneTest.cs
@@ -1,38 +1,71 @@
 ﻿using Matrix.Xml;
+using Shouldly;
 using Xunit;
-
 
 namespace Matrix.Tests.Xmpp.Tune
 {
-    
     public class TuneTest
     {
-        const string XML1 = @"<tune xmlns='http://jabber.org/protocol/tune'>
-          <artist>Yes</artist>
-          <length>686</length>
-          <rating>8</rating>
-          <source>Yessongs</source>
-          <title>Heart of the Sunrise</title>
-          <track>3</track>
-          <uri>http://www.yesworld.com/lyrics/Fragile.html#9</uri>
-        </tune>";
-        
         [Fact]
-        public void Test1()
+        public void XmlShouldBeOfTypeTune()
         {
-            var xmpp1 = XmppXElement.LoadXml(XML1);
-            Assert.Equal(true, xmpp1 is Matrix.Xmpp.Tune.Tune);
+            XmppXElement.LoadXml(Resource.Get("Xmpp.Tune.tune1.xml"))
+                .ShouldBeOfType<Matrix.Xmpp.Tune.Tune>();
+        }
 
-            var tune1 = xmpp1 as Matrix.Xmpp.Tune.Tune;
-            Assert.Equal(tune1.Artist, "Yes");
-            Assert.Equal(tune1.Length, 686);
-            Assert.Equal(tune1.Rating, 8);
-            Assert.Equal(tune1.Source, "Yessongs");
-            Assert.Equal(tune1.Title, "Heart of the Sunrise");
-            Assert.Equal(tune1.Track, "3");
-            Assert.Equal(tune1.Uri.ToString(), "http://www.yesworld.com/lyrics/Fragile.html#9");
+        [Fact]
+        public void TestArtist()
+        {
+            XmppXElement.LoadXml(Resource.Get("Xmpp.Tune.tune1.xml"))
+                .Cast<Matrix.Xmpp.Tune.Tune>()
+                .Artist.ShouldBe("Yes");
+        }
 
-            var tune = new Matrix.Xmpp.Tune.Tune
+        [Fact]
+        public void TestLength()
+        {
+            XmppXElement.LoadXml(Resource.Get("Xmpp.Tune.tune1.xml"))
+                .Cast<Matrix.Xmpp.Tune.Tune>()
+                .Length.ShouldBe(686);
+        }
+
+        [Fact]
+        public void TestRating()
+        {
+            XmppXElement.LoadXml(Resource.Get("Xmpp.Tune.tune1.xml"))
+                .Cast<Matrix.Xmpp.Tune.Tune>()
+                .Rating.ShouldBe(8);
+        }
+
+        [Fact]
+        public void TestSource()
+        {
+            XmppXElement.LoadXml(Resource.Get("Xmpp.Tune.tune1.xml"))
+                .Cast<Matrix.Xmpp.Tune.Tune>()
+                .Source.ShouldBe("Yessongs");
+        }
+
+        [Fact]
+        public void TestTrack()
+        {
+            XmppXElement.LoadXml(Resource.Get("Xmpp.Tune.tune1.xml"))
+                .Cast<Matrix.Xmpp.Tune.Tune>()
+                .Track.ShouldBe("3");
+        }
+
+        [Fact]
+        public void TestUri()
+        {
+            XmppXElement.LoadXml(Resource.Get("Xmpp.Tune.tune1.xml"))
+                .Cast<Matrix.Xmpp.Tune.Tune>()
+                .Uri.ToString().ShouldBe("http://www.yesworld.com/lyrics/Fragile.html#9");
+        }
+
+        [Fact]
+        public void BuildTune()
+        {
+            var expectedXml = Resource.Get("Xmpp.Tune.tune1.xml");
+            new Matrix.Xmpp.Tune.Tune
             {
                 Artist = "Yes",
                 Length = 686,
@@ -41,11 +74,8 @@ namespace Matrix.Tests.Xmpp.Tune
                 Title = "Heart of the Sunrise",
                 Track = "3",
                 Uri = new System.Uri("http://www.yesworld.com/lyrics/Fragile.html#9")
-            };
-
-            tune.ShouldBe(XML1);
+            }
+            .ShouldBe(expectedXml);
         }
-
-      
     }
 }
