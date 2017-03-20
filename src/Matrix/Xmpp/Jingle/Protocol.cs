602083d7d3a4b4e7f9db2aa19cf1b3f57f900902
Alex <gnauck@gmail.com>
2017-03-20 19:40:28 +0100

    update license headers

--- src/Matrix/Xmpp/Jingle/Protocol.cs
+++ src/Matrix/Xmpp/Jingle/Protocol.cs
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2003-2017 by AG-Software <info@ag-software.de>
+ *
+ * All Rights Reserved.
+ *
+ * This file is part of the MatriX project.
+ *
+ * NOTICE: All information contained herein is, and remains the property
+ * of AG-Software and its suppliers, if any.
+ * The intellectual and technical concepts contained herein are proprietary
+ * to AG-Software and its suppliers and may be covered by German and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from AG-Software.
+ *
+ * Contact information for AG-Software is available at http://www.ag-software.de
+ */
+
 namespace Matrix.Xmpp.Jingle
 {
     public enum Protocol
