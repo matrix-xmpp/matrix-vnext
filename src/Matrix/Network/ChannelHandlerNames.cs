10afa5a702a82574c3cfa99cf1f0b603746496d1
Alex <gnauck@gmail.com>
2017-04-03 21:43:15 -0700

    add ChannelHandlerNames

---
+++ src/Matrix/Network/ChannelHandlerNames.cs
@@ -0,0 +1,18 @@
+﻿namespace Matrix.Network
+{
+    public class ChannelHandlerNames
+    {
+        public static string DisconnetHandler           => "DisconnectHandler";
+        public static string ZlibDecoder                => "ZlibDecoder";
+        public static string ZlibEncoder                => "ZlibEncoder";
+        public static string KeepAliveHandler           => "KeepAliveHandler";
+        public static string XmlStreamDecoder           => "XmlStreamDecoder";
+        public static string XmppXElementEncoder        => "XmppXElementEncoder";
+        public static string UTF8StringEncoder          => "UTF8StringEncoder";              
+        public static string XmppPingHandler            => "XmppPingHandler";
+        public static string XmppStreamEventHandler     => "XmppStreamEventHandler";
+        public static string StreamFooterHandler        => "StreamFooterHandler";
+        public static string XmppStanzaHandler          => "XmppStanzaHandler";
+        public static string CatchAllXmppStanzaHandler  => "CatchAllXmppStanzaHandler";
+    }
+}
