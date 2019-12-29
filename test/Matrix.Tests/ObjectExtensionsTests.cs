8dba6301f0b5e3f5a73e2e633aec3a963b83a957
Alexander Gnauck <alex@ag-software.net>
2019-12-29 20:41:58 +0000

    Merged PR 79: feat(type extension): add IsAnyOf object extension method
    Add IsAnyOf type extension to simplify code which needs to check types like the following:
    
    ```
    if (iq.Type == IqType.Result || iq.Type == IqType.Set)
    {
       // do something
    }
    ```
    versus
    
    ```
    if (iq.Type.IsAnyOf(IqType.Set, IqType.Result)
    {
       // do something
    }
    ```

--- test/Matrix.Tests/ObjectExtensionsTests.cs
+++ test/Matrix.Tests/ObjectExtensionsTests.cs
@@ -20,15 +20,16 @@
  */
 
 using DotNetty.Transport.Bootstrapping;
-using Matrix;
 using Matrix.Network;
 using Matrix.Network.Resolver;
 using Shouldly;
 using Xunit;
-using System.Linq;
 
 namespace Matrix.Tests
 {
+    using Matrix.Xmpp;
+    using Matrix.Xmpp.Client;
+
     public class ObjectExtensionsTests
     {
         [Fact]
@@ -41,6 +42,47 @@ namespace Matrix.Tests
 
             string s = "hello world";
             s.Implements<IDirectTls>().ShouldBe(false);
+        }
+
+
+        [Fact]
+        public void Int_IsAnyOf_Should_Return_True()
+        {
+            1.IsAnyOf(0, 2, 3, 4, 5, 6, 1).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void String_IsAnyOf_Values_Should_Return_True()
+        {
+            "foo".IsAnyOf("foo", "bar", "foobar").ShouldBeTrue();
+        }
+
+        [Fact]
+        public void String_IsAnyOf_Values_Should_Return_False()
+        {
+            "foo".IsAnyOf("foooo", "bar", "foobar").ShouldBeFalse();
+        }
+
+        [Fact]
+        public void IqType_IsAnyOf_Values_Should_Return_True()
+        {
+            var iq = new Iq
+            {
+                Type = IqType.Result
+            };
+
+            iq.Type.IsAnyOf(IqType.Set, IqType.Result).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void IqType_IsAnyOf_Values_Should_Return_False()
+        {
+            var iq = new Iq
+            {
+                Type = IqType.Result
+            };
+
+            iq.Type.IsAnyOf(IqType.Error, IqType.Get).ShouldBeFalse();
         }
     }
 }
